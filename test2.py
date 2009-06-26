64f9af231aab8c2947cdd9f811c3891f750da888
forrest <forrest@470744a7-cac9-478e-843e-5ec1b25c69e8>
2009-06-26 03:02:19 +0000

    test2
    git-svn-id: svn://forre.st/pyopenal@664 470744a7-cac9-478e-843e-5ec1b25c69e8

---
+++ test2.py
@@ -0,0 +1,31 @@
+import sys
+import time
+import math
+import random
+import os
+
+import openal
+
+device = openal.Device()
+contextlistener = device.ContextListener()
+source = openal.Source()
+buffer = openal.Buffer('sound.wav')
+
+contextlistener.position = 0, 0, 0
+contextlistener.velocity = 0, 0, 0
+contextlistener.orientation = 0, 1, 0, 0, 0, 1
+
+source.buffer = buffer
+source.looping = True
+source.gain = 1
+source.play()
+
+t = 0
+amplitude = 1
+freq = 1
+while True:
+    source.position = amplitude*math.sin(t*freq), amplitude*math.cos(t*freq), 0
+    source.velocity = amplitude*freq*math.cos(t*freq), -amplitude*freq*math.sin(t*freq), 0
+    print (t*180/math.pi)%360
+    time.sleep(.01)
+    t += .01
