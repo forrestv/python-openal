4c26a057e1e843a6a9ee5d04db3e0a353e3fb127
Forrest Voight <forrest@forre.st>
2012-04-16 23:39:25 -0400

    moved generator tool to /tools

--- openal/mk/gengen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./gen.sh al > ../_al.py
-./gen.sh alc > ../_alc.py
-./gen.sh alut > ../_alut.py
---
+++ tools/gengen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+./gen.sh al > ../openal/_al.py
+./gen.sh alc > ../openal/_alc.py
+./gen.sh alut > ../openal/_alut.py
